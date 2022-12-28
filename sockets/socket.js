d07bd0ed617316189fb72d81c729fcb4e1e1a305
Thiago Gobbi <Thiagomgobbi7@gmail.com>
2022-12-28 10:37:58 -0300

    Fin seccion 3

---
+++ sockets/socket.js
@@ -0,0 +1,22 @@
+const {io} = require('../index');
+
+
+//Mensajes de Sockets
+io.on('connect', client => {   
+    console.log('Cliente conectado')
+
+
+    client.on('disconnect', () => { 
+        console.log('Cliente desconectado')
+     });
+
+     client.on('mensaje', (payload)=> {
+        console.log('mensaje!!', payload);
+
+        io.emit('mensaje', {admin:'Nuevo mensaje'});
+
+
+     });
+
+
+  });
